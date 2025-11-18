6247368110c5ba35eae2beea123891a7a1c21dc3
ShinichiroFunatsu <sfunatsu.git@gmail.com>
2025-11-18 16:05:42 +0900

    feat: add withStyle import

--- app/src/main/java/com/example/scopedstorage/MainActivity.kt
+++ app/src/main/java/com/example/scopedstorage/MainActivity.kt
@@ -47,6 +47,7 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
 import androidx.core.content.ContextCompat
 import coil.ImageLoader
